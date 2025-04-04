48681333a47e3641044d82dd38b1fe59064bb46d
Kiera <11018737@uvu.edu>
2025-04-03 19:56:10 -0600

    got that emission running

--- Scripting 3 Challenge/Assets/Pixel Adventure 1/Assets/scripts/TriggerParticleEffect.cs
+++ Scripting 3 Challenge/Assets/Pixel Adventure 1/Assets/scripts/TriggerParticleEffect.cs
@@ -6,17 +6,34 @@ using UnityEngine;
 public class TriggerParticleEffect : MonoBehaviour
 {
     private ParticleSystem particleSystem;
-    public int particleAmount = 10; 
+
+    public int firstEmissionAmount = 10;
+    public int secondEmissionAmount = 20;
+    public int thirdEmissionAmount = 30;
+    public float delayBetweenParticles = 0.5f;
     private void Start()
     {
        particleSystem = GetComponent<ParticleSystem>(); 
     }
 
-   private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<CharacterController>())
         {
-            particleSystem.Emit(particleAmount);
+            StartCoroutine(EmitParticlesCoroutine());
         }
+    }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        particleSystem.Emit(firstEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenParticles);
+        
+        particleSystem.Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenParticles);
+        
+        particleSystem.Emit(thirdEmissionAmount);
+        
+        Destroy(gameObject);
     }
 }
